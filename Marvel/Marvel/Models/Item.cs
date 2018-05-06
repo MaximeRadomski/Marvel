8864e2d69c8a98e9ab9e212540b91a502721466f
Abjec <maxime.radomski@gmail.com>
2018-05-06 09:34:44 +0200

    Project Creation

---
+++ Marvel/Marvel/Models/Item.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace Marvel.Models
+{
+    public class Item
+    {
+        public string Id { get; set; }
+        public string Text { get; set; }
+        public string Description { get; set; }
+    }
+}
---
+++ Marvel/Marvel/ViewModels/ItemDetailViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+using Marvel.Models;
+
+namespace Marvel.ViewModels
+{
+    public class ItemDetailViewModel : BaseViewModel
+    {
+        public Item Item { get; set; }
+        public ItemDetailViewModel(Item item = null)
+        {
+            Title = item?.Text;
+            Item = item;
+        }
+    }
+}
